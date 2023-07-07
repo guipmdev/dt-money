774b2505421363f1657ba8c29c38805184352cde
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-07 10:53:56 -0300

    Estrutura visual - Setup do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
+
+  body {
+    background: ${(props) => props.theme['gray-800']};
+
+    color: ${(props) => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Roboto, sans-serif;
+  }
+`
