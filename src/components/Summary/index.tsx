b2b2cca41a7b21079a9ae43e1676fda89cb6fc2b
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-07-08 16:04:46 -0300

    Conexão com a API - Formatando valores

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,8 @@ import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
 
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 
+import { priceFormatter } from '../../utils/formatter'
+
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
@@ -33,7 +35,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -43,7 +45,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard $variant="green">
@@ -53,7 +55,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
